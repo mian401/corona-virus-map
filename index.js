c1c3c5de7b4e0b400859f842233ee1996aa4103c
Mian Muhammad Ishaq <73196168+mian401@users.noreply.github.com>
2021-08-16 21:54:14 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function updatemap() {
+
+    fetch("/data.json")
+        .then(response => response.json())
+        .then((rsp) => {
+
+            rsp.data.forEach(element => {
+
+                // retrieving data from the jsomn file            
+
+                let latitude = element.latitude;
+                let longitude = element.longitude;
+
+                //Adding A Marker
+                if (element.infected < 500) {
+                    new mapboxgl.Marker({ color: "#33ff77" })
+                        .setLngLat([longitude, latitude])
+                        .addTo(map);
+                }
+                
+                else if (element.infected >= 500 && element.infected <= 1000) {
+                    new mapboxgl.Marker({ color: " #66c2ff" })
+                        .setLngLat([longitude, latitude])
+                        .addTo(map);
+                }
+                
+                else if (element.infected > 1000) {
+                    new mapboxgl.Marker({ color: "#ff1a1a" })
+                        .setLngLat([longitude, latitude])
+                        .addTo(map);
+                }
+
+            });
+        });
+}
+
+updatemap();
